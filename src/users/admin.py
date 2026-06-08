9a46a7960144fac438ab07fb76b470b72984b602
-- <smoke571reg@yandex.ru>
2026-06-08 22:10:29 +0000

    Added display of email_verified field in Django Admin

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from .models import User, EmailVerificationToken
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "email_verified",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
 
 
-@admin.register(EmailVerificationToken)
 class EmailVerificationTokenAdmin(admin.ModelAdmin):
     list_display = ("user", "token", "status", "expires_at", "sent_at")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(EmailVerificationToken, EmailVerificationTokenAdmin)
